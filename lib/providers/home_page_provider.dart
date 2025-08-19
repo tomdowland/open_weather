62c934ccade3e0fb9c590d1517c82b5e61ae3e10
tomdowland <thomasd@baleen.studio>
2025-08-19 18:07:51 +0900

    provider adjustments, use new providers in view notifier

--- lib/providers/home_page_provider.dart
+++ lib/providers/home_page_provider.dart
@@ -3,7 +3,8 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:open_weather/models/current_weather.dart';
 import 'package:open_weather/models/enum/error.dart';
 import 'package:open_weather/models/forecast_data.dart';
-import 'package:open_weather/providers/async_weather_provider.dart';
+import 'package:open_weather/providers/async_current_weather.dart';
+import 'package:open_weather/providers/async_five_day_forecast.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'home_page_provider.freezed.dart';
 part 'home_page_provider.g.dart';
@@ -25,38 +26,43 @@ abstract class FrontPage with _$FrontPage {
 class HomePageNotifier extends _$HomePageNotifier {
   @override
   FrontPage build() {
-    final asyncWeather = ref.watch(asyncWeatherProvider);
     try {
-      return FrontPage(
-        weatherResults: asyncWeather.value?.fiveDayForecast,
-        currentWeather: asyncWeather.value?.currentWeather,
-        isBusy: asyncWeather.isLoading,
-        hasError: asyncWeather.hasError,
-        errorType: asyncWeather.value?.errorType,
-        errorMessage: asyncWeather.value?.errorMessage,
-      );
-    } catch (e) {
+      final asyncToday = ref.watch(asyncCurrentWeatherProvider);
+      final asyncForecast = ref.watch(asyncFiveDayForecastProvider);
+        return FrontPage(
+          weatherResults: asyncForecast.value,
+          currentWeather: asyncToday.value,
+          isBusy: asyncForecast.isLoading || asyncToday.isLoading,
+          hasError: asyncForecast.hasError || asyncToday.hasError,
+          // errorType: asyncWeather.value?.errorType,
+          // errorMessage: asyncWeather.value?.errorMessage,
+        );
+    } on Exception catch (e) {
       return FrontPage(
         isBusy: false,
-        hasError: asyncWeather.hasError,
-        errorType: asyncWeather.value?.errorType,
-        errorMessage: asyncWeather.value?.errorMessage,
+        hasError: true,
+        errorType: RequestError.networkError,
+        errorMessage: e.toString(),
       );
     }
   }
 
+
+
   Future<void> searchCity(String city) async {
-    final asyncWeather = ref.watch(asyncWeatherProvider);
     try {
       state = state.copyWith(isBusy: true, hasError: false);
-      final result = await ref
-          .read(asyncWeatherProvider.notifier)
-          .fetchWeatherByCity(city);
+      final forecast = await ref
+          .read(asyncFiveDayForecastProvider.notifier)
+          .searchForecast(city);
+      final today = await ref
+          .read(asyncCurrentWeatherProvider.notifier)
+          .searchCurrentWeather(city);
       state = state.copyWith(
         editing: false,
-        weatherResults: result?.fiveDayForecast,
-        currentWeather: result?.currentWeather,
-        errorType: result?.errorType,
+        weatherResults: forecast,
+        currentWeather: today,
+        // errorType: result?.errorType,
         isBusy: false,
         hasError: false,
       );
@@ -66,14 +72,14 @@ class HomePageNotifier extends _$HomePageNotifier {
           editing: false,
           isBusy: false,
           hasError: true,
-          errorType: asyncWeather.value?.errorType,
+          errorType: RequestError.notFound,
         );
       } else {
         state = state.copyWith(
           editing: false,
           isBusy: false,
-          hasError: false,
-          errorType: asyncWeather.value?.errorType,
+          hasError: true,
+          // errorType: asyncWeather.value?.errorType,
         );
       }
       rethrow;
